055b91dcdc0d032840fadddc7539414651b747b3
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-07-07 00:24:20 +0900

    Add posting article function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,14 @@ class ArticlesController < ApplicationController
 	def new
 		@Article = Article.new
 	end
+
+	def create
+		Article.create(title: article_params[:title], text: article_params[:text], user_id: 1)
+		redirect_to action: :index
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
 end
