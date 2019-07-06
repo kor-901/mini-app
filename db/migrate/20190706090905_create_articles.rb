85369f9dd822cd801fec553f7c3f6a2cdce82cbd
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-07-06 18:18:03 +0900

    Add title column to articles table

--- db/migrate/20190706090905_create_articles.rb
+++ db/migrate/20190706090905_create_articles.rb
@@ -2,6 +2,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
       t.integer :user_id
+      t.string :title
       t.text :text
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2019_07_06_090905) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
+    t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
