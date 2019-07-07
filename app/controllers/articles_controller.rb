5261082217a74c327c7e4c91f235187a3fb5b342
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-07-07 17:33:24 +0900

    Create update action of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,12 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def update
+		article = Article.find(params[:id])
+		article.update(article_params) if article.user_id == current_user.id
+		redirect_to action: :index
+	end
+
 	private
 	def article_params
 		params.require(:article).permit(:title, :text)
