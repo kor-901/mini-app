faa587bce997fabcdc931ae19a17d06f5e07a8b1
kor-901 <52075363+kor-901@users.noreply.github.com>
2019-07-07 16:29:10 +0900

    Create edit action of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,10 @@ class ArticlesController < ApplicationController
 		redirect_to action: :index
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	private
 	def article_params
 		params.require(:article).permit(:title, :text)
